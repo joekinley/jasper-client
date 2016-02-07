c8f1b176c8dc4d45941e67d453ebb9ff056dbf73
joekinley <rafael@tweep.de>
2016-02-07 20:00:37 +0000

    FIX: I dont like python

--- client/listener.py
+++ client/listener.py
@@ -40,7 +40,7 @@ class Listener(object):
                 mic.say(data[4:])
                 #print(data[4:])
             elif data.startswith("listen!"):
-                //#answer = mic.activeListen()
+                #answer = mic.activeListen()
                 msg = new Message(self, "listen")
                 self.queue.put(msg)
                 self.queue.join()
